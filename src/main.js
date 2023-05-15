2b919ba2c7c35e6e7a9151c86d33d3ff11832f9e
HASAN SAKALLI <89208116+hasansakalli@users.noreply.github.com>
2023-05-15 10:20:19 +0200

    main.js

--- src/main.js
+++ src/main.js
@@ -12,13 +12,13 @@ import axios from 'axios';
 axios.defaults.baseURL = "https://fixture-register-default-rtdb.europe-west1.firebasedatabase.app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZjX9O3cKVsIfvjIML8Jiy8FhnFyf_1dQ",
-  authDomain: "fixture-register.firebaseapp.com",
-  databaseURL: "https://fixture-register-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fixture-register",
-  storageBucket: "fixture-register.appspot.com",
-  messagingSenderId: "229293818467",
-  appId: "1:229293818467:web:62e69e712724ca66a42e56"
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 
 initializeApp(firebaseConfig)
